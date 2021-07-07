bc8e5b0edd4c72ddd6f269f9eff5c486f6dda236
xlashdt <73990956+xlashdt@users.noreply.github.com>
2021-07-07 17:07:10 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Xbeauty</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Xbeauty" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          
         </p>
       </main>
 
